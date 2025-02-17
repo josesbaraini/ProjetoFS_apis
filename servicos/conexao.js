a93ea926fb18a6e22a944c1e6655178ed16a7e19
josesbaraini <josesbarainips@gmail.com>
2025-02-17 14:29:25 -0400

    uyg

--- servicos/conexao.js
+++ servicos/conexao.js
@@ -4,7 +4,7 @@ import dotenv from "dotenv";
 dotenv.config();
 
 const pool = mysql.createPool({
-    port:3307,
+    port:process.env.DB_PORT,
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
     password: process.env.DB_PASS,
