64953301e2c445e5f478d67d0f8f6c1c183893b5
José Sbaraini <josesbarainips@gmail.com>
2025-06-20 14:13:49 -0400

    Enhance authentication response to include user information

--- rotas/userRotas.js
+++ rotas/userRotas.js
@@ -47,7 +47,7 @@ router.post("/login", async (req, res) => {
 });
 
 router.get('/autenticar', autenticar, (req, res) => {
-    res.json({ mensagem: "Dados Autenticados" ,ok:true})
+    res.json({ mensagem: "Dados Autenticados" ,ok:true, usuario: req.user})
 })
 
 router.post('/cadastro', async (req, res) => {
